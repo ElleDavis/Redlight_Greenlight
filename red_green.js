5459c5c8902198ba98f17ca010289c9be2e486cb
Elle Davis <ShanitriceDavis@gmail.com>
2022-05-15 23:01:46 -0400

    gitpages working

--- red_green.js
+++ red_green.js
@@ -5,31 +5,19 @@ const colors=[
     "red",
     "green",
     "yellow",
+    c1.textContent=document.getElementById("c1").style.backgroundColor="green",
+    c1.textContent=document.getElementById("c1").style.backgroundColor="red",
+    c1.textContent=document.getElementById("c1").style.backgroundColor="yellow"
 ]
-
+// c1.textContent=document.getElementById("c1").style.backgroundColor="green"
+// c1.textContent=document.getElementById("c1").style.backgroundColor="red"
+// c1.textContent=document.getElementById("c1").style.backgroundColor="yellow"
 // get random color
 let colorArray=colors[Math.floor(Math.random()* colors.length)]
 let circle=document.getElementById("c1")
 
-//set player 
-// let currentPlayer=document.getElementById("currentPlayer")
-// currentPlayer.innerHTML=1
-// console.log(currentPlayer)
-
 //set player function to change
 // function setPlayer(){
-
-// let currentPlayer=document.getElementById("currentPlayer")
-// currentPlayer.innerHTML=1
-// console.log(currentPlayer)
-
-//     if ( currentPlayer.innerHTML=1 && speedTix.textContent <= 3){
-//         // console.log(currentPlayer.innerHTML=1)
-//         console.log("monday")
-//     } else if (currentPlayer=1 && speedTix >= 3){
-//     //    console.log(currentPlayer.innerHTML= 2)
-//         console.log("tuesday")
-//      } else { alert("somethings off")}
 //     }
 
 
@@ -48,61 +36,76 @@ function goPressed(){
     let speedingTicket=document.querySelector("#speedingTicket")
     // speedingTicket.textContent="speeding Tickets:"
     speedingTicket="speeding Tickets:"
+
 }
 
 
+
+//starts player one and changes player 2| keep both player 1 and 2 scores | show winner
+ 
+
+//try switch case for color
+// switch (c1.innerHTML==="green"){
+//     case c1="green":
+//         document.getElementById("c1").style.backgroundColor="green"
+//         console.log('show green');
+//         break;
+//     case c1="red": 
+//     document.getElementById("c1").style.backgroundColor="red"
+//         console.log('show Red');
+//         break;
+//         case c1="yellow": 
+//         document.getElementById("c1").style.backgroundColor="yellow"
+//             console.log('show yellow');
+//             break;
+//     default:
+//         console.log('Not Found!');
+// }
+
+// let currentPlayer=document.getElementById("currentPlayer")
+// currentPlayer.innerHTML=1
+
+// function goPressed(){
+//     switch (c1.innerHTML){
+//         case c1.innerHTML="green":
+//             document.getElementById("c1").style.color="green"
+//             console.log('show green');
+//             break;
+//         case c1.textContent="red": 
+//         document.getElementById("c1").style.color="red"
+//             console.log('show Red');
+//             break;
+//             case c1.textContent="yellow": 
+//             document.getElementById("c1").style.color="yellow"
+//                 console.log('show yellow');
+//                 break;
+//         default:
+//             console.log('Not Found!');
+//     }
+
+// }
+
+
 //add 1 to score and tix when clicked | add 1 to score when c1= green| add 1 to tix when not green |
-//stats player one and changes player 2| keep both player 1 and 2 scores | show winner
-// works perf
-//function greenPressed(){
-//     let currentPlayer=document.getElementById("currentPlayer")
-//     currentPlayer.innerHTML=1
-//     console.log(currentPlayer)
-
-//       if(speedingTicket.innerHTML=== "3"){
-//         alert(currentPlayer.innerHTML="Player 2")
-//         console.log(currentPlayer)
-
-//       } else if ( c1.textContent ===`${"green"}`){
-//             let total=document.getElementById("add")
-//             total.textContent = Number(total.textContent) +1  
-//             console.log("hello")
-//         } else {
-//             let speedTix=document.getElementById("speedingTicket")
-//              speedTix.textContent= Number(speedTix.textContent) +1
-//              console.log("bye");
-//         }
-//     }          
-
-
+//starts player 1 & changes to player 2|reset points and tix and add for player two but no stop
 function greenPressed(){
-    let currentPlayer=document.getElementById("currentPlayer")
-    currentPlayer.innerHTML=1
-    console.log(currentPlayer)
+let currentPlayer=document.getElementById("currentPlayer")
+currentPlayer.innerHTML=1
 
-    if(speedingTicket.innerHTML=== "3"){
-        add.textContent=""
-        speedingTicket.textContent=""
-        alert(currentPlayer.innerHTML="Ready Player 2 ")
-        console.log(currentPlayer)
-    } else if( currentPlayer.innerHTML="Player 2" && c1.textContent ===`${"green"}`){
-        let total=document.getElementById("add")
-        total.textContent = Number(total.textContent) +1  
-        console.log("hello2")
-    } else if(currentPlayer.innerHTML="Player 2" && c1.textContent !==`${"green"}` ){
-        let speedTix=document.getElementById("speedingTicket")
-         speedTix.textContent= Number(speedTix.textContent) +1
-         console.log("bye2");
-    } else if ( c1.textContent ===`${"green"}`){
-            let total=document.getElementById("add")
-            total.textContent = Number(total.textContent) +1  
-            console.log("hello")
-    } else if (c1.textContent !==`${"green"}`){
-            let speedTix=document.getElementById("speedingTicket")
-             speedTix.textContent= Number(speedTix.textContent) +1
-             console.log("bye")}
-        }
-    
+if(speedingTicket.innerHTML=== "3"){
+    add.textContent=""
+    speedingTicket.textContent=""
+    alert(currentPlayer.innerHTML="Ready Player 2 ")
+} else if ( c1.textContent ===`${"green"}`){
+    let total=document.getElementById("add")
+    total.textContent = Number(total.textContent) +1  
+    console.log("hello")
+} else if (c1.textContent !==`${"green"}`){
+    let speedTix=document.getElementById("speedingTicket")
+    speedTix.textContent= Number(speedTix.textContent) +1
+    console.log("bye")
+} 
+    }
 
 
 //Reset Button
@@ -119,8 +122,8 @@ function resetPressed(){
 
 // EVENTS
 //Set player
-    const displayCurrentPlayer=document.querySelector("#currentPlayer")
-    displayCurrentPlayer.addEventListener("check", setPlayer)
+    // const displayCurrentPlayer=document.querySelector("#currentPlayer")
+    // displayCurrentPlayer.addEventListener("check", setPlayer)
 
 // click green button and add piont if circle is green
     const greenButton=document.querySelector("#add")
