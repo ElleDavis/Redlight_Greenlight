391302efe69ad28e5727bd251697403f3880504e
Elle Davis <ShanitriceDavis@gmail.com>
2022-05-16 09:03:33 -0400

    remove border, change go color and blink

--- red_green.js
+++ red_green.js
@@ -18,14 +18,14 @@ let circle=document.getElementById("c1")
 
 //Go Button open the scoreboard(point & tix) | start color in Intervals
 function goPressed(){
-    let colorArray=setInterval(function(){c1.textContent =`${(colors[Math.floor(Math.random()* colors.length)])}`}, 2000);
+    let colorArray=setInterval(function(){c1.textContent =`${(colors[Math.floor(Math.random()* colors.length)])}`}, 1000);
     c1.textContent=`${colorArray}`
 
 //Scoreboard opens when go is clicked
     let totalScore=document.querySelector("#add")
     // totalScore.textContent="score:"
     totalScore="score:"
-    
+
 //Speeding tix opens when go is clicked
     let speedingTicket=document.querySelector("#speedingTicket")
     // speedingTicket.textContent="speeding Tickets:"
@@ -56,22 +56,80 @@ currentPlayer.innerHTML=1
 //             default:
 //                 console.log('Not Found!');
 //         }
+// worst case submission: gets 3 tix or 3 pts turn to player2
+// if (speedingTicket.innerHTML >= "3"){
+//     add.textContent=""
+//     speedingTicket.textContent=""
+//     alert("LOSER, Try Again!")
+//     (currentPlayer.innerHTML="Ready Player 2")
+// }else if (add.innerHTML==="3" ){
+//     alert("WINNER")
+// } else if ( c1.textContent ===`${"green"}`){
+//     let total=document.getElementById("add")
+//     total.textContent = Number(total.textContent) +1  
+//     console.log("hello")
+// } else if (c1.textContent !==`${"green"}`){
+//     let speedTix=document.getElementById("speedingTicket")
+//     speedTix.textContent= Number(speedTix.textContent) +1
+//     console.log("bye")
+// } 
 
-if (speedingTicket.innerHTML >= "3"){
+// //checks if 3 tixs| clears points tix|alert loser
+// if (speedingTicket.innerHTML >= "3"){
+//     add.textContent=""
+//     speedingTicket.textContent=""
+//     alert("LOSER, Try Again!")
+//     (currentPlayer.innerHTML="Ready Player 2")
+// //if 3 points alert winner
+// }else if (add.innerHTML==="3" ){
+//     add.textContent=""
+//     speedingTicket.textContent=""
+//     alert("WINNER")
+// // if player2 and c1 is green add point
+// }else if (currentPlayer.innerHTML==="Ready Player 2" && c1.textContent !==`${"green"}`){
+//     alert("Play2 Tixx")
+// // if player2 and c1 is not green add tixx 
+// }else if (currentPlayer.innerHTML==="Ready Player 2" && c1.textContent ===`${"green"}`){
+//     alert("Player2 score")
+// // if player1 and c1 is green add point 
+// } else if ( c1.textContent ===`${"green"}`){
+//     let total=document.getElementById("add")
+//     total.textContent = Number(total.textContent) +1  
+//     console.log("hello")
+// // if player1 and c1 is not green add tix
+// } else if (c1.textContent !==`${"green"}`){
+//     let speedTix=document.getElementById("speedingTicket")
+//     speedTix.textContent= Number(speedTix.textContent) +1
+//     console.log("bye")
+// } 
+//if player1 and c1 is green add tix
+if (currentPlayer.innerHTML==2 ){
+    alert("start player 2")
+// if player2 and c1 is not green add tixx 
+} else if (currentPlayer.innerHTML===2 && c1.textContent ===`${"green"}`){
+    alert("Player2 score")
+} else if (currentPlayer.innerHTML===2 &&  c1.textContent !==`${"green"}`){
+    alert("Player2 Tix")
+// if player1 and c1 is green add point 
+} else if (speedingTicket.innerHTML==="3"){
     add.textContent=""
     speedingTicket.textContent=""
     alert("LOSER, Try Again!")
-    (currentPlayer.innerHTML="Ready Player 2")
-}else if (add.innerHTML==="3" ){
+    currentPlayer.textContent= 2
+} else if (add.innerHTML==="3" ){
+    add.textContent=""
+    speedingTicket.textContent=""
     alert("WINNER")
-} else if ( c1.textContent ===`${"green"}`){
+} else if (currentPlayer.innerHTML==1 && c1.textContent ===`${"green"}`){
     let total=document.getElementById("add")
-    total.textContent = Number(total.textContent) +1  
+    total.textContent = Number(total.textContent) +1 
     console.log("hello")
-} else if (c1.textContent !==`${"green"}`){
+        // if player1 and c1 is not green add tix
+} else if (currentPlayer.innerHTML==1 && c1.textContent !==`${"green"}`){
     let speedTix=document.getElementById("speedingTicket")
     speedTix.textContent= Number(speedTix.textContent) +1
     console.log("bye")
+        // 
 } 
     }
 
